b864a2e82ec16810efa0c630bc2e3912993a4f27
Petar Stakic <petar.stakic@enetelsolutions.com>
2018-02-05 09:36:38 +0100

    Added new implementation of GreetingService and marked it as primary. Refactored MyController class to use this bean by default. Reflection on property name in PropertyInjectedController did not work after adding primary service, so we added a Qualifier annotation to specify which service bean we want.

--- src/main/java/rs/enentel/didemo/controllers/MyController.java
+++ src/main/java/rs/enentel/didemo/controllers/MyController.java
@@ -2,12 +2,20 @@ package rs.enentel.didemo.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import rs.enentel.didemo.services.GreetingService;
+
 @Controller
 public class MyController {
 	
+	private GreetingService greetingService;
+	
+	public MyController(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+
 	public String hello() {
 		System.out.println("Hello!!! ");
 		
-		return "foo";
+		return greetingService.sayGreeting();
 	}
 }
